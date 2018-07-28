27b9fd7d219412723ae832588ef2d7287a381d07
Mujahid Islam Peal <mujahid.islam_peal@outlook.com>
2018-07-28 10:29:19 +0600

    Adding Category Setup UI

---
+++ StockManagementSystem/CategorySetupUI.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace StockManagementSystem
+{
+    partial class CategorySetupUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.categoryDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(106, 57);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(271, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(302, 83);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // categoryDataGridView
+            // 
+            this.categoryDataGridView.AllowUserToAddRows = false;
+            this.categoryDataGridView.AllowUserToDeleteRows = false;
+            this.categoryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.categoryDataGridView.Location = new System.Drawing.Point(51, 128);
+            this.categoryDataGridView.Name = "categoryDataGridView";
+            this.categoryDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.categoryDataGridView.Size = new System.Drawing.Size(326, 150);
+            this.categoryDataGridView.TabIndex = 3;
+            // 
+            // CategorySetupUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 328);
+            this.Controls.Add(this.categoryDataGridView);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "CategorySetupUI";
+            this.Text = "Category Setup";
+            ((System.ComponentModel.ISupportInitialize)(this.categoryDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.DataGridView categoryDataGridView;
+    }
+}
+
--- StockManagementSystem/CategorySetupUI.cs
+++ StockManagementSystem/CategorySetupUI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StockManagementSystem
 {
-    public partial class Form1 : Form
+    public partial class CategorySetupUI : Form
     {
-        public Form1()
+        public CategorySetupUI()
         {
             InitializeComponent();
         }
