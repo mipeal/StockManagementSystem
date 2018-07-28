48a139c53a285d8a1e1dfc8b603382fd89249494
Mujahid Islam Peal <mujahid.islam_peal@outlook.com>
2018-07-28 11:42:21 +0600

    Adding UI for View By Dates

--- StockManagementSystem/ItemsSummaryUI.Designer.cs
+++ StockManagementSystem/ItemsSummaryUI.Designer.cs
@@ -100,7 +100,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "ItemsSummaryUI";
-            this.Text = "ItemsSummaryUI";
+            this.Text = "Items Summary";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ StockManagementSystem/ViewSalesByDatesUI.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace StockManagementSystem
+{
+    partial class ViewSalesByDatesUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fromDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.toDatePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(107, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(590, 276);
+            this.dataGridView1.TabIndex = 28;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(362, 68);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 27;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "From";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "To";
+            // 
+            // fromDatePicker
+            // 
+            this.fromDatePicker.CustomFormat = "mm/dd/yyyy";
+            this.fromDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fromDatePicker.Location = new System.Drawing.Point(290, 30);
+            this.fromDatePicker.Name = "fromDatePicker";
+            this.fromDatePicker.Size = new System.Drawing.Size(99, 20);
+            this.fromDatePicker.TabIndex = 29;
+            this.fromDatePicker.Value = new System.DateTime(2018, 7, 28, 0, 0, 0, 0);
+            // 
+            // toDatePicker
+            // 
+            this.toDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.toDatePicker.Location = new System.Drawing.Point(421, 30);
+            this.toDatePicker.Name = "toDatePicker";
+            this.toDatePicker.Size = new System.Drawing.Size(100, 20);
+            this.toDatePicker.TabIndex = 30;
+            this.toDatePicker.Value = new System.DateTime(2018, 7, 28, 0, 0, 0, 0);
+            // 
+            // ViewSalesByDatesUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toDatePicker);
+            this.Controls.Add(this.fromDatePicker);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ViewSalesByDatesUI";
+            this.Text = "View Sales Between Two Dates";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker fromDatePicker;
+        private System.Windows.Forms.DateTimePicker toDatePicker;
+    }
+}
