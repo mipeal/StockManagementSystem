74ef8fa8684e0c7276f66c773735fda3ff7b77bb
Mujahid Islam Peal <mujahid.islam_peal@outlook.com>
2018-07-30 10:29:56 +0600

    Adding Model Classes for Inventory

---
+++ StockManagementSystem/Models/InventoryModels/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.Models.InventoryModels
+{
+    class Inventory
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int StockAvailable { get; set; }
+        public int StockTransaction { get; set; }
+        public int StockType { get; set; }
+        public int StockOutType { get; set; }
+        public DateTime TransactionDate { get => DateTime.Now; }
+
+    }
+}
---
+++ StockManagementSystem/Models/InventoryModels/StockOutTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.Models.InventoryModels
+{
+    enum StockOutTypes
+    {
+        sell=1,
+        damage=2,
+        lost=3
+    }
+}
